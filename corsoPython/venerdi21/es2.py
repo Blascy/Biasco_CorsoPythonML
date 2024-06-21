16da654c621e0b64021519cd49957a8363ba6fcd
Blascy <101145394+Blascy@users.noreply.github.com>
2024-06-21 14:49:10 +0200

    Add files via upload

---
+++ corsoPython/venerdi21/es2.py
@@ -0,0 +1,8 @@
+#scrivi programma che chieda all'utente di inserire una parola e poi utilizzi un ciclo for per stampare ogni lettera della parola su una nuova riga
+# Chiedi all'utente di inserire una parola
+parola = input("Inserisci una parola: ")
+
+# Utilizza un ciclo for per iterare attraverso ogni lettera della parola
+for lettera in parola:
+    # Stampa ogni lettera su una nuova riga
+    print(lettera)
